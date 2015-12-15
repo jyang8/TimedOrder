a487944c2f39facb3fe5c195927102ff97eaa346
Jack Schluger <jschluger@stuy.edu>
2015-12-14 22:04:10 -0500

    testing for times

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -139,6 +139,7 @@ public class OrderedArrayList {
     // main method solely for testing purposes
     public static void main( String[] args ) 
     {
+	/*
 	OrderedArrayList Franz = new OrderedArrayList();
 
 	System.out.println("\nValues to add via addLinear() calls:");
@@ -159,34 +160,80 @@ public class OrderedArrayList {
 	for( int i = 0; i < 16; i++ ) {
 	    System.out.println( i + ": " + Franz.findLin(i));
 	}
+*/
 
-
-	Franz = new OrderedArrayList();
+	OrderedArrayList Franz = new OrderedArrayList();
 
 	System.out.println("\nValues to add via addBinary() calls:");
 
-				
+	//so we are consistant
+	int num = 100000;
+       
 	// testing binary search
-	for( int i = 0; i < 15; i++ ) {
-	    int valToAdd = i; //(int)( 50 * Math.random() );
-	    System.out.println( valToAdd );
+	for( int i = 0; i < num; i++ ) {
+	    int valToAdd = (int)( num * Math.random() );
 	    Franz.addBinary( valToAdd );
 	}
-
+	/*
 	System.out.println("\nafter population via addBinary() calls:");
 	System.out.println( Franz );
 	System.out.println();
         
-
+	
 	System.out.println("\nsearch via findBin() calls:");
 	for( int i = 0; i < 16; i++ ) {
 	    System.out.println( i + ": " + Franz.findBin(i));
 	}
 
-	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	   INSERT WELL-COMMENT TIMING APPARATUS HERE
-	   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-    }
+	*/
+	//avg time for lin search
+	long linTimeAvg = 0;
+
+	//# of searches done (so we can take the right average)
+	long linTimeCtr = 0;
+
+	//avg time for lin search
+	long binTimeAvg = 0;
+
+	//# of searches done (so we can take the right average)
+	long binTimeCtr = 0;
+
+	//time before
+	long start = 0;
+	//time after
+	long finish = 0;
+	//time during
+	long temp = 0;
+
+	//random number, must store it so it is the same for both searches
+	int rand = 0;
+
+	for (int i = 0;  i < num; i++){
+	    rand = (int)( num * Math.random() );
+	    start = System.currentTimeMillis();
+	    Franz.findLin(rand);
+	    finish = System.currentTimeMillis();
+	    
+	    temp = finish - start;
+	    linTimeAvg = (linTimeAvg * linTimeCtr + temp) / (linTimeCtr + 1) ;
+	    linTimeCtr += 1;
+
+	    start = System.currentTimeMillis();
+	    Franz.findBin(rand);
+	    finish = System.currentTimeMillis();
+	    
+	    temp = finish - start;
+	    binTimeAvg = (binTimeAvg * binTimeCtr + temp) / (binTimeCtr + 1) ;
+	    binTimeCtr += 1;
+	}
+	
+	String s = "Average linear search time: ";
+	System.out.println(s + linTimeAvg);
+
+	s = "Average binary search time: ";
+	System.out.println(s + binTimeAvg);
+
+    }//end main()
 
 }//end class OrderedArrayList
  
